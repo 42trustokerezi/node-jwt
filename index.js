2009c08eaf683e3954a8512e59d9267d8bffa432
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-26 00:38:22 +0100

    DB setup

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+mongoose = require('mongoose');
+
+//connect to DB 
+mongoose.connect('mongodb://localhost/node-auth', ()=> console.log('connected to db'));
 
 //Import Routes
 const authRoute = require('./routes/auth');
 
-app.listen(9000, ()=> console.log('Server online'));
+//Route middlewares
+app.use('/api/user', authRoute)
+
+app.listen(8000, ()=> console.log('Server online'));
 
