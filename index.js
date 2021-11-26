2b9d21811dfb124ed3db46e4c8038db09a940ca1
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-26 22:53:18 +0100

    final commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 //Import Routes
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 //connect to DB 
 mongoose.connect(
@@ -18,7 +19,8 @@ mongoose.connect(
 app.use(express.json());
 
 //Route middlewares
-app.use('/api/user', authRoute)
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute)
 
 app.listen(8000, ()=> console.log('Server online'));
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation } = require('../validation');
 
 
 
@@ -14,18 +16,41 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
+
+    //Hash Passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //CREATE A NEW USER
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user:user._id });
     }catch(err){
         res.status(400).send(err);
     }
 });
 
+//LOGIN
+router.post('/login', async(req, res) => {
+    //LETS VALIDATE THE DATA BEFORE WE LOGIN A USER
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if user is already in the database
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email is not found');
+
+    //If Password is Correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Password is Invalid');
+
+    //Create and assing a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+});
 module.exports = router;
