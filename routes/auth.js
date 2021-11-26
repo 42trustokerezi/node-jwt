7af1ac26a946652e05c0eb29d2b259bbe83168b4
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-26 19:07:35 +0100

    checking if user exist in DB

--- routes/auth.js
+++ routes/auth.js
@@ -10,17 +10,22 @@ router.post('/register', async (req, res) => {
     const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-   const user = new User({
-       name: req.body.name,
-       email: req.body.email,
-       password: req.body.password
-   });
-   try{
-       const savedUser = await user.save();
-       res.send(savedUser);
-   }catch(err){
-       res.status(400).send(err);
-   }
+    //Checking if user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    //CREATE A NEW USER
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
