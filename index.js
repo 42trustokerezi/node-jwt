697322908ef134826f6fd316df4bb8ea748bcbad
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-26 18:32:00 +0100

    data validation

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 mongoose = require('mongoose');
 
-//connect to DB 
-mongoose.connect('mongodb://localhost/node-auth', ()=> console.log('connected to db'));
+dotenv.config();
 
 //Import Routes
 const authRoute = require('./routes/auth');
+
+//connect to DB 
+mongoose.connect(
+    process.env.DB_CONNECT, 
+    ()=> console.log('connected to db')
+);
+
+//Middleware
+app.use(express.json());
 
 //Route middlewares
 app.use('/api/user', authRoute)
